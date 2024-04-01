6ce3282672ef7425bd02cfe687c04337b59e6a7f
Kritim Bastola <kbastola1@unm.edu>
2024-04-01 16:24:07 -0600

    Made classes and defined their variables

---
+++ src/MyBigInteger.java
@@ -0,0 +1,15 @@
+public class MyBigInteger {
+    IntegerNode sign;
+
+    public MyBigInteger(String n){
+
+    }
+
+
+    public class IntegerNode{
+        int digits;
+
+        IntegerNode higherPositions;
+
+    }
+}
